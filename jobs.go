c866763fbf2984b582e8375a4feca0cf9af91a4c
Ian Calvert <ianjcalvert@gmail.com>
2013-09-15 17:58:00 +0100

    Multi jobs, multi children

--- jobs.go
+++ jobs.go
@@ -1,29 +1,36 @@
 package main
 
 type Job struct {
-    dependencies [](chan bool)
-    output (chan bool)
+	dependencies [](chan bool)
+	children     [](chan bool)
 }
 
 func NewJob() *Job {
-    job := Job{
-        dependencies: make([](chan bool), 0, 100),
-        output: make(chan bool),
-    }
-    return &job
+	job := Job{
+		dependencies: make([](chan bool), 0, 100),
+		children:     make([](chan bool), 0, 100),
+	}
+	return &job
 }
 
-func (job *Job) AddDependency(dependency chan bool) {
-    job.dependencies = append(job.dependencies, dependency)
+func (job *Job) AddDependency(dependency *Job) {
+	c := make(chan bool)
+	job.dependencies = append(job.dependencies, c)
+	dependency.children = append(dependency.children, c)
+}
+
+func (job *Job) AddListener(output chan bool) {
+	job.children = append(job.children, output)
 }
 
 func (job *Job) SetProcess(function func()) {
-    go func() {
-        for i := 0; i < len(job.dependencies); i++ {
-            <-job.dependencies[i]
-        }
-        function()
-        job.output <- true
-    }()
+	go func() {
+		for i := 0; i < len(job.dependencies); i++ {
+			<-job.dependencies[i]
+		}
+		function()
+		for i := 0; i < len(job.children); i++ {
+			job.children[i] <- true
+		}
+	}()
 }
-
--- jobs_test.go
+++ jobs_test.go
@@ -2,23 +2,17 @@ package main
 
 import (
 	"testing"
-        "fmt"
 )
 
-func TestOutputWhenSingleDependencyFinished(t *testing.T) {
-	dependency := make(chan bool)
+func TestJobWithNoDependenciesRuns(t *testing.T) {
+	success := make(chan bool)
 	job_run := false
 	job := NewJob()
-	job.AddDependency(dependency)
+	job.AddListener(success)
 	job.SetProcess(func() {
 		job_run = true
 	})
-	if job_run {
-		t.Errorf("Job should not run before dependency finishes")
-	}
-	dependency <- true
-	success := <-job.output
-	if !success {
+	if !(<-success) {
 		t.Errorf("Job should have returned true")
 	}
 	if !job_run {
@@ -27,59 +21,41 @@ func TestOutputWhenSingleDependencyFinished(t *testing.T) {
 
 }
 
-func TestOutputWithMultipleDependencies(t *testing.T) {
-	dependency1 := make(chan bool)
-	dependency2 := make(chan bool)
-	dependency3 := make(chan bool)
-	job_run := false
-	job := NewJob()
-	job.AddDependency(dependency1)
-	job.AddDependency(dependency2)
-	job.AddDependency(dependency3)
-	job.SetProcess(func() {
-		job_run = true
+func TestOutputWithSingleDependency(t *testing.T) {
+	success := make(chan bool)
+	job1_run := false
+	job2_run := false
+	job1 := NewJob()
+	job2 := NewJob()
+	job2.AddDependency(job1)
+	job2.AddListener(success)
+	job1.SetProcess(func() {
+		job1_run = true
 	})
-	if job_run {
-		t.Errorf("Job should not run before dependency finishes")
-	}
-	dependency1 <- true
-	dependency2 <- true
-	dependency3 <- true
-	success := <-job.output
-	if !success {
+	job2.SetProcess(func() {
+		job2_run = true
+	})
+	if !(<-success) {
 		t.Errorf("Job should have returned true")
 	}
-	if !job_run {
+	if !(job1_run && job2_run) {
 		t.Errorf("Job should actually have been run")
 	}
 }
 
 func TestChainedJobs(t *testing.T) {
-	dependency1 := make(chan bool)
-	dependency2 := make(chan bool)
-	dependency3 := make(chan bool)
+	success := make(chan bool)
 	job1 := NewJob()
-	job1.SetProcess(func() {
-            fmt.Printf("Job1\n")
-	})
 	job2 := NewJob()
-	job2.SetProcess(func() {
-            fmt.Printf("Job2\n")
-	})
 	job3 := NewJob()
-	job3.SetProcess(func() {
-            fmt.Printf("Job3\n")
-	})
-	job1.AddDependency(dependency1)
-	job2.AddDependency(dependency2)
-        job2.AddDependency(job1.output)
-        job3.AddDependency(job2.output)
-        job3.AddDependency(dependency3)
-        dependency1 <- true
-        dependency2 <- true
-        dependency3 <- true
-	success := <-job3.output
-	if !success {
+	job2.AddDependency(job1)
+	job3.AddDependency(job2)
+	job3.AddDependency(job1)
+        job3.AddListener(success)
+	job1.SetProcess(func() {})
+	job2.SetProcess(func() {})
+	job3.SetProcess(func() {})
+	if !(<-success) {
 		t.Errorf("Job should have returned true")
 	}
 }
