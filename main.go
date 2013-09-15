1f60f86d03daf0897be68e6f7a73be67dd172476
Ian Calvert <ianjcalvert@gmail.com>
2013-09-15 18:57:20 +0100

    Actually run commands in parallel

--- main.go
+++ main.go
@@ -1,53 +1,63 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"os"
 	"os/exec"
+        "strconv"
 )
 
-func run(cmd *exec.Cmd, outputname string) {
-	fo, err := os.Create("/tmp/" + outputname + ".out")
-	ferr, err := os.Create("/tmp/" + outputname + ".err")
-	defer func() {
-		if err := fo.Close(); err != nil {
-			panic(err)
+func create_runner(cmd *exec.Cmd, outputname string) func() {
+	return func() {
+		fo, err := os.Create("/tmp/" + outputname + ".out")
+		ferr, err := os.Create("/tmp/" + outputname + ".err")
+		defer func() {
+			if err := fo.Close(); err != nil {
+				panic(err)
+			}
+			if err := ferr.Close(); err != nil {
+				panic(err)
+			}
+		}()
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
 		}
-		if err := ferr.Close(); err != nil {
-			panic(err)
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			log.Fatal(err)
 		}
-	}()
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
+		if err := cmd.Start(); err != nil {
+			log.Fatal(err)
+		}
+		io.Copy(fo, stdout)
+		io.Copy(fo, stderr)
+		err = cmd.Wait()
 	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
-	}
-	io.Copy(fo, stdout)
-	io.Copy(fo, stderr)
-	err = cmd.Wait()
 }
 
-func sleep(c chan<- string, fname string) {
-	cmd := exec.Command("echo", "hello")
-	run(cmd, fname)
-	c <- "I'm done"
+func sleeper(time int, name string) func(){
+	cmd := exec.Command("sleep", strconv.Itoa(time))
+	return create_runner(cmd, name)
 }
 
 func main() {
-	c := make(chan string)
-	for i := 0; i < 10; i++ {
-		go sleep(c, fmt.Sprintf("%d", i))
-	}
-	for i := 0; i < 10; i++ {
-		result := <-c
-		log.Printf("Received %v", result)
+        totalJobs := 3
+	c := make(chan bool)
+        j1 := NewJob()
+        j2 := NewJob()
+        j3 := NewJob()
+        j2.AddDependency(j1)
+        j3.AddDependency(j1)
+        j1.AddListener(c)
+        j2.AddListener(c)
+        j3.AddListener(c)
+        j1.SetProcess(sleeper(5, "uno"))
+        j2.SetProcess(sleeper(5, "dos"))
+        j3.SetProcess(sleeper(5, "tres"))
+	for i := 0; i < totalJobs; i++ {
+		<-c
+		log.Printf("Job %d/%d finished", i+1, totalJobs)
 	}
 }
