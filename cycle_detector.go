650c799a8d95fc801183aa2d471f2c24d8fc5334
Ian Calvert <ianjcalvert@gmail.com>
2013-09-16 20:24:12 +0100

    refactor, remove comment, complex test

--- cycle_detector.go
+++ cycle_detector.go
@@ -1,131 +1,87 @@
 package main
 
-import (
-    "fmt"
-)
-
 type Node struct {
-    children []*Node
-    index int
-    lowlink int
+	children []*Node
+	index    int
+	lowlink  int
 }
 
 func NewNode() *Node {
-    node := Node {
-        children : []*Node{},
-    }
-    return &node
+	node := Node{
+		children: []*Node{},
+	}
+	return &node
 }
 
 func (parent *Node) AddChild(child *Node) {
-    parent.children = append(parent.children, child)
+	parent.children = append(parent.children, child)
 }
-/*
-algorithm tarjan is
-  input: graph G = (V, E)
-  output: set of strongly connected components (sets of vertices)
 
-  index := 0
-  S := empty
-  for each v in V do
-     if (v.index is undefined) then
-      strongconnect(v)
-    end if
-  end for
-
-  function strongconnect(v)
-    // Set the depth index for v to the smallest unused index
-    v.index := index
-    v.lowlink := index
-    index := index + 1
-    S.push(v)
-
-    // Consider successors of v
-     for each (v, w) in E do
-       if (w.index is undefined) then
-         // Successor w has not yet been visited; recurse on it
-        strongconnect(w)
-        v.lowlink  := min(v.lowlink, w.lowlink)
-      else if (w is in S) then
-         // Successor w is in stack S and hence in the current SCC
-         v.lowlink  := min(v.lowlink, w.index)
-      end if
-    end for
-
-    // If v is a root node, pop the stack and generate an SCC
-    if (v.lowlink = v.index) then
-      start a new strongly connected component
-      repeat
-        w := S.pop()
-        add w to current strongly connected component
-      until (w = v)
-      output the current strongly connected component
-    end if
-  end function
-*/
-
-func min(a int, b int) (int) {
-    if a < b {
-        return a
-    }
-    return b
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func present(a *Node, list []*Node) bool {
-    for _, b := range list {
-        if b == a {
-            return true
-        }
-    }
-    return false
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
-func HasCycle(graph []*Node) (bool) {
-        sscs := []([]*Node){}
-        s := []*Node{}
-        index := 1
-        var strongconnect func(v *Node)
-        strongconnect = func(v *Node) {
-            v.index = index
-            v.lowlink = index
-            index += 1
-            s = append(s, v)
-            for _, w := range v.children {
-                if w.index == 0 {
-                    strongconnect(w)
-                    v.lowlink = min(v.lowlink, w.lowlink)
-                } else if present(w, s) {
-                    v.lowlink = min(v.lowlink, w.index)
-                }
-            }
-            if (v.lowlink == v.index) {
-                //Loopy bit
-                fmt.Printf("SSC START\n")
-                ssc := []*Node{}
-                w := s[len(s)-1]
-                s = append(s[:len(s)-1])
-                fmt.Printf("Adding something to an ssc\n")
-                ssc = append(ssc, w)
-                for v != w {
-                    w = s[len(s)-1]
-                    s = append(s[:len(s)-1])
-                    fmt.Printf("Adding something to an ssc\n")
-                    ssc = append(ssc, w)
-                }
-                fmt.Printf("SSC END: %v\n", ssc)
-                sscs = append(sscs, ssc)
-            }
-        }
-        for _, v := range graph {
-            fmt.Printf("%v\n",v.index)
-            if v.index == 0 {
-                strongconnect(v)
-            }
-        }
-        for _, ssc := range sscs {
-            if len(ssc) > 1 {
-                return true
-            }
-        }
-    return false;
+func getLargeSSCS(graph []*Node) []([]*Node) {
+	sscs := []([]*Node){}
+	s := []*Node{}
+	index := 1
+	var strongconnect func(v *Node)
+	strongconnect = func(v *Node) {
+		v.index = index
+		v.lowlink = index
+		index += 1
+		s = append(s, v)
+		for _, w := range v.children {
+			if w.index == 0 {
+				strongconnect(w)
+				v.lowlink = min(v.lowlink, w.lowlink)
+			} else if present(w, s) {
+				v.lowlink = min(v.lowlink, w.index)
+			}
+		}
+		if v.lowlink == v.index {
+			ssc := []*Node{}
+			w := s[len(s)-1]
+			s = append(s[:len(s)-1])
+			ssc = append(ssc, w)
+			for v != w {
+				w = s[len(s)-1]
+				s = append(s[:len(s)-1])
+				ssc = append(ssc, w)
+			}
+			sscs = append(sscs, ssc)
+		}
+	}
+	for _, v := range graph {
+		if v.index == 0 {
+			strongconnect(v)
+		}
+	}
+	large_sscs := []([]*Node){}
+	for _, ssc := range sscs {
+		if len(ssc) > 1 {
+			large_sscs = append(large_sscs, ssc)
+		}
+	}
+	return large_sscs
+}
+
+func HasCycle(graph []*Node) bool {
+	large_sscs := getLargeSSCS(graph)
+	if len(large_sscs) > 0 {
+		return true
+	}
+	return false
 }
--- cycle_detector_test.go
+++ cycle_detector_test.go
@@ -5,29 +5,55 @@ import (
 )
 
 func TestSimpleCycleIsDetected(t *testing.T) {
-        a := NewNode()
-        b := NewNode()
-        c := NewNode()
-        a.AddChild(b)
-        b.AddChild(c)
-        c.AddChild(a)
+	a := NewNode()
+	b := NewNode()
+	c := NewNode()
+	a.AddChild(b)
+	b.AddChild(c)
+	c.AddChild(a)
 
-        graph := []*Node{a,b,c}
-        if !HasCycle(graph) {
-            t.Errorf("Cycle A->B->C->A not detected")
-        }
+	graph := []*Node{a, b, c}
+	if !HasCycle(graph) {
+		t.Errorf("Cycle A->B->C->A not detected")
+	}
 }
 
 func TestNoCycleDetectedInTree(t *testing.T) {
-        a := NewNode()
-        b := NewNode()
-        c := NewNode()
-        a.AddChild(b)
-        b.AddChild(c)
-        a.AddChild(c)
+	a := NewNode()
+	b := NewNode()
+	c := NewNode()
+	a.AddChild(b)
+	b.AddChild(c)
+	a.AddChild(c)
 
-        graph := []*Node{a,b,c}
-        if HasCycle(graph) {
-            t.Errorf("No cycle present, but dected in A->B, A->C")
-        }
+	graph := []*Node{a, b, c}
+	if HasCycle(graph) {
+		t.Errorf("No cycle present, but dected in A->B, A->C")
+	}
+}
+
+func TestComplexCyclesWithBranching(t *testing.T) {
+	a := NewNode()
+	b := NewNode()
+	c := NewNode()
+	d := NewNode()
+	e := NewNode()
+	f := NewNode()
+	g := NewNode()
+	h := NewNode()
+	i := NewNode()
+	a.AddChild(b)
+	b.AddChild(c)
+	c.AddChild(d)
+	d.AddChild(e)
+	e.AddChild(b)
+	i.AddChild(h)
+	h.AddChild(g)
+	f.AddChild(b)
+	e.AddChild(h)
+
+	graph := []*Node{a, b, c, d, e, f, g, h, i}
+	if !HasCycle(graph) {
+		t.Errorf("Complex cycles not detected")
+	}
 }
